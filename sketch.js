7c826819472ffe48660ecf708ad036edd3d637b2
WC9 <eashwar.sp2007@gmail.com>
2021-02-11 14:29:19 +0530

    Changes

--- sketch.js
+++ sketch.js
@@ -19,13 +19,13 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(70,320,70,70);
+    box2 = new Box(960,320,70,70);
+    pig1 = new Pig(810, 370);
+    log1 = new Log(810,560,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box3 = new Box(70,40,70,70);
+    box4 = new Box(920,40,70,70);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
